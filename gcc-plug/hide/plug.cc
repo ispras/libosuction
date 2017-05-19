ab685535efea18126d579c81f21bc1511ec64650
Vladislav Ivanishin <vlad@ispras.ru>
2017-05-19 13:44:54 +0300

    Rename the pass to drop-globally-unused

--- gcc-plug/hide/plug.cc
+++ gcc-plug/hide/plug.cc
@@ -11,10 +11,10 @@ int plugin_is_GPL_compatible;
 
 namespace {
 
-const pass_data pass_data_sweep_externally_useless =
+const pass_data pass_data_drop_globally_unused =
 {
   SIMPLE_IPA_PASS, /* type */
-  "sweep-externally-useless", /* name */
+  "drop-globally-unused", /* name */
   OPTGROUP_NONE, /* optinfo_flags */
   TV_NONE, /* tv_id */
   0, /* properties_required */ // TODO: PROP_cfg ?
@@ -24,19 +24,19 @@ const pass_data pass_data_sweep_externally_useless =
   0, /* todo_flags_finish */
 };
 
-class pass_sweep_externally_useless : public simple_ipa_opt_pass
+class pass_drop_globally_unused : public simple_ipa_opt_pass
 {
 public:
-  pass_sweep_externally_useless (gcc::context *ctxt)
-    : simple_ipa_opt_pass (pass_data_sweep_externally_useless, ctxt)
+  pass_drop_globally_unused (gcc::context *ctxt)
+    : simple_ipa_opt_pass (pass_data_drop_globally_unused, ctxt)
   {}
 
   virtual unsigned int execute (function *);
 
-}; // class pass_sweep_externally_useless
+}; // class pass_drop_globally_unused
 
 unsigned int
-pass_sweep_externally_useless::execute (function *)
+pass_drop_globally_unused::execute (function *)
 {
   cgraph_node *node;
 
@@ -50,9 +50,9 @@ pass_sweep_externally_useless::execute (function *)
 } // anon namespace
 
 static simple_ipa_opt_pass *
-make_pass_sweep_externally_useless (gcc::context *ctxt)
+make_pass_drop_globally_unused (gcc::context *ctxt)
 {
-  return new pass_sweep_externally_useless (ctxt);
+  return new pass_drop_globally_unused (ctxt);
 }
 
 int
@@ -67,7 +67,7 @@ plugin_init (plugin_name_args *i, plugin_gcc_version *v)
       return 1;
     }
 
-  pass_info.pass = make_pass_sweep_externally_useless (g);
+  pass_info.pass = make_pass_drop_globally_unused (g);
   pass_info.reference_pass_name = "materialize-all-clones";
   pass_info.ref_pass_instance_number = 1;
   pass_info.pos_op = PASS_POS_INSERT_AFTER;
