eefd73cd577b168e3566dc6a51a813b705af81f3
Vladislav Ivanishin <vlad@ispras.ru>
2017-05-23 19:50:31 +0300

    Remove unneeded "drop-globally-unused" pass

--- gcc-plug/hide/plug.cc
+++ gcc-plug/hide/plug.cc
@@ -11,65 +11,6 @@ int plugin_is_GPL_compatible;
 
 namespace {
 
-const pass_data pass_data_drop_globally_unused =
-{
-  SIMPLE_IPA_PASS, /* type */
-  "drop-globally-unused", /* name */
-  OPTGROUP_NONE, /* optinfo_flags */
-  TV_NONE, /* tv_id */
-  0, /* properties_required */ // TODO: PROP_cfg ?
-  0, /* properties_provided */
-  0, /* properties_destroyed */
-  0, /* todo_flags_start */
-  0, /* todo_flags_finish */
-};
-
-class pass_drop_globally_unused : public simple_ipa_opt_pass
-{
-public:
-  pass_drop_globally_unused (gcc::context *ctxt)
-    : simple_ipa_opt_pass (pass_data_drop_globally_unused, ctxt)
-  {}
-
-  virtual unsigned int execute (function *);
-
-}; // class pass_drop_globally_unused
-
-static bool
-unused_p (cgraph_node *node)
-{
-  return false;
-}
-
-unsigned int
-pass_drop_globally_unused::execute (function *)
-{
-  cgraph_node *node;
-  cgraph_node *next;
-
-  for (node = symtab->first_function (); node; node = next)
-    {
-      next = symtab->next_function (node);
-
-      if (unused_p (node))
-	node->remove ();
-      else
-	printf ("%s\n", node->name ());
-    }
-  return 0;
-}
-
-} // anon namespace
-
-static simple_ipa_opt_pass *
-make_pass_drop_globally_unused (gcc::context *ctxt)
-{
-  return new pass_drop_globally_unused (ctxt);
-}
-
-
-namespace {
-
 const pass_data pass_data_hide_globally_invisible =
 {
   SIMPLE_IPA_PASS, /* type */
@@ -132,18 +73,11 @@ plugin_init (plugin_name_args *i, plugin_gcc_version *v)
   plugin_init_func check = plugin_init;
   struct register_pass_info pass_info;
 
-  if (!plugin_default_version_check (&gcc_version ,v))
+  if (!plugin_default_version_check (&gcc_version, v))
     {
       fprintf (stderr, "GCC version does not match plugin's.");
       return 1;
     }
-
-  pass_info.pass = make_pass_drop_globally_unused (g);
-  pass_info.reference_pass_name = "materialize-all-clones";
-  pass_info.ref_pass_instance_number = 1;
-  pass_info.pos_op = PASS_POS_INSERT_AFTER;
-
-  register_callback (i->base_name, PLUGIN_PASS_MANAGER_SETUP, NULL, &pass_info);
 
   pass_info.pass = make_pass_hide_globally_invisible (g);
   pass_info.reference_pass_name = "visibility";
