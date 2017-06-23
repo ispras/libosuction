8ae3fb3d10a4b96de60f140306f7f334ec2b3bce
Vladislav Ivanishin <vlad@ispras.ru>
2017-06-23 17:54:37 +0300

    Generalize for compatibility with gcc 4.9 - 7.1

--- gcc-plug/hide/plug.cc
+++ gcc-plug/hide/plug.cc
@@ -1,5 +1,6 @@
 /* Copyright (C) 2017 Vladislav Ivanishin */
 
+#include "bversion.h"
 #include "gcc-plugin.h"
 #include "tree.h"
 #include "tree-pass.h"
@@ -8,6 +9,10 @@
 #include "cgraph.h"
 #include "splay-tree.h"
 #include "diagnostic-core.h"
+
+#if BUILDING_GCC_VERSION < 4009
+#error "This gcc is too old. The minimum required version is 4.9.x"
+#endif
 
 int plugin_is_GPL_compatible;
 
@@ -18,6 +23,10 @@ const pass_data pass_data_hide_globally_invisible =
   SIMPLE_IPA_PASS, /* type */
   "hide-globally-invisible", /* name */
   OPTGROUP_NONE, /* optinfo_flags */
+#if BUILDING_GCC_VERSION == 4009
+  false, /* has_gate */
+  true, /* has_execute */
+#endif
   TV_NONE, /* tv_id */
   0, /* properties_required */ // TODO: PROP_cfg ?
   0, /* properties_provided */
@@ -25,6 +34,31 @@ const pass_data pass_data_hide_globally_invisible =
   0, /* todo_flags_start */
   0, /* todo_flags_finish */
 };
+
+#if BUILDING_GCC_VERSION >= 6000
+#define transparent_alias_p(node) node->transparent_alias
+#else
+#define transparent_alias_p(node) false
+#endif
+
+#if BUILDING_GCC_VERSION >= 5000
+#define _EXECUTE_ARGS function *
+#define fatal_error(...) fatal_error (UNKNOWN_LOCATION, __VA_ARGS__)
+#define set_comdat_group(node, group) node->set_comdat_group (group)
+#define set_section(node, section) node->set_section (section)
+#define make_decl_local(node) node->make_decl_local ()
+#define dissolve_same_comdat_group_list(node) \
+  node->dissolve_same_comdat_group_list ()
+#else
+#define _EXECUTE_ARGS void
+#define fatal_error(...) fatal_error (__VA_ARGS__)
+#define set_comdat_group(node, group) DECL_COMDAT_GROUP (node->decl) = group
+#define set_section(node, section)
+#define make_decl_local(node) symtab_make_decl_local (node->decl)
+#define dissolve_same_comdat_group_list(node) \
+  symtab_dissolve_same_comdat_group_list (node)
+#endif
+
 
 class pass_hide_globally_invisible : public simple_ipa_opt_pass
 {
@@ -34,7 +68,7 @@ public:
     fname (NULL)
   {}
 
-  virtual unsigned int execute (function *);
+  virtual unsigned int execute (_EXECUTE_ARGS);
 
   void set_fname (const char *fname_) { fname = fname_; }
 
@@ -80,8 +114,7 @@ read_decl_names (FILE *f, int num_nodes, splay_tree nodes)
   for (i = 0; i < num_nodes; i++)
     {
       if (fscanf (f, "%ms", &decl_name) != 1)
-	fatal_error (UNKNOWN_LOCATION,
-		     "error reading static/libprivate symbol names");
+	fatal_error ("error reading static/libprivate symbol names");
 
       splay_tree_insert (nodes, (splay_tree_key) decl_name, 0);
     }
@@ -95,7 +128,7 @@ pass_hide_globally_invisible::read_vis_changes (void)
 
   f = fopen (fname, "r");
   if (!f)
-    fatal_error (UNKNOWN_LOCATION, "cannot open %s", fname);
+    fatal_error ("cannot open %s", fname);
 
   fscanf (f, "%d %d", &num_statics, &num_libpriv);
   read_decl_names (f, num_statics, static_nodes);
@@ -122,16 +155,16 @@ pass_hide_globally_invisible::localize_comdat (symtab_node *node)
   do
     {
       next = cur->same_comdat_group;
-      cur->set_comdat_group (NULL);
+      set_comdat_group (cur, NULL_TREE);
       if (!cur->alias)
-	cur->set_section (NULL);
-      if (!cur->transparent_alias)
-	cur->make_decl_local ();
+	set_section (cur, NULL);
+      if (!transparent_alias_p (cur))
+	make_decl_local (cur);
       cur = next;
     }
   while (cur != node);
 
-  node->dissolve_same_comdat_group_list ();
+  dissolve_same_comdat_group_list (node);
 
   return true;
 }
@@ -146,7 +179,7 @@ dont_hide_p (const symtab_node *node)
 }
 
 unsigned int
-pass_hide_globally_invisible::execute (function *)
+pass_hide_globally_invisible::execute (_EXECUTE_ARGS)
 {
   symtab_node *node;
   bool comdat_priv_failed_p;
@@ -165,7 +198,7 @@ pass_hide_globally_invisible::execute (function *)
 
       if (no_external_uses_p (node))
 	{
-	  gcc_assert (!node->transparent_alias);
+	  gcc_assert (!transparent_alias_p (node));
 
           if (!TREE_PUBLIC (node->decl))
             continue;
@@ -176,7 +209,7 @@ pass_hide_globally_invisible::execute (function *)
           if (node->same_comdat_group)
             comdat_priv_failed_p = !localize_comdat (node);
 	  else
-	    node->make_decl_local ();
+	    make_decl_local (node);
 	}
       if ((lib_private_p (node) || comdat_priv_failed_p)
 	  && !dont_hide_p (node))
@@ -222,8 +255,7 @@ plugin_init (plugin_name_args *i, plugin_gcc_version *v)
 	break;
       }
   if (!fname)
-    fatal_error (UNKNOWN_LOCATION,
-		 "file with visibility modifications not specified"
+    fatal_error ("file with visibility modifications not specified "
 		 "(pass 'fname' argument to the plugin)");
 
   pass_info.pass = make_pass_hide_globally_invisible (g, fname);
