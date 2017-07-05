1d7658a229643d5b905b02e1f097cbe51c7d2133
Vladislav Ivanishin <vlad@ispras.ru>
2017-07-05 14:18:11 +0300

    Fix make check
    Rely on nm instead of objdump where we can.

--- gcc-plug/hide/check-bin.sh
+++ gcc-plug/hide/check-bin.sh
@@ -12,11 +12,10 @@ pass()
 }
 
 # staticfoo an unused function,
-# staticvar is only used locally and thus shouldn't get a name (?)
 # libprivate call should not go through plt
-(objdump -d test-libpriv.so | egrep "(foo|staticvar|\blibprivate@plt)") && fail
+(objdump -d test-libpriv.so | egrep "(staticfoo|\blibprivate@plt)") && fail
 
 # Uppercase letters in nm output mean global symbol visibility.
-(nm test-libpriv.so | grep libpriv | grep [A-Z]) && fail
+(nm test-libpriv.so | egrep "(libpriv|staticvar)" | grep [A-Z]) && fail
 
 pass
