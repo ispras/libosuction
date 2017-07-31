341cfb68ac8962e2e816a42e880b8dcf710ad2a1
Evgeny <kudryashov@ispras.ru>
2017-07-31 14:45:41 +0300

    dlsym-plugin: unify test scripts

--- gcc-plug/dlsym/tests/jfunc-test.sh
+++ gcc-plug/dlsym/tests/jfunc-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PLUGIN_ARG="-fplugin-arg-libplug-sign-dlsym=1 -fplugin-arg-libplug-in=signs.txt -fplugin-arg-libplug-run=0"
 
+COUNTER_PASSED=0
+COUNTER_TOTAL=0
+
 function compile_test() {
   echo "COMPILING $TFILE"
   $TCOMPILER $TFILE $TFLAGS
@@ -14,10 +17,12 @@ function dump_check_test() {
   local name="$1"
   local str="$2"
   local expected="$3"
+  COUNTER_TOTAL=$((COUNTER_TOTAL + 1))
   printf "TESTING $TFILE ($name):\t"
   local actual=$(dump_contains $TFILE.*i.$TPASS "$str")
   if [ $expected -eq $actual ]
   then
+    COUNTER_PASSED=$((COUNTER_PASSED + 1))
     echo "PASS"
   else
     echo "FAIL: Expected $expected but actual is $actual"
@@ -28,7 +33,7 @@ function cleanup_test() {
   rm $TFILE.* *.out
 }
 
-# jfunc--pass.c
+# jfunc-pass.c
 TPASS="jfunc"
 echo "----------------"
 echo "jfunc-pass tests"
@@ -70,5 +75,11 @@ dump_check_test "jf1" "_ZN12interseption22GetRealFunctionAddressEPKcd,0->_ZN12in
 dump_check_test "jf2" "_ZN12interseption22GetRealFunctionAddressEPKcS1_,0->_ZN12interseption22GetRealFunctionAddressEPKc,0" 1
 dump_check_test "jf3" "_ZN12interseption22GetRealFunctionAddressEPKc,0->dlsym,1" 1
 cleanup_test
+
+# Summary
+echo "----------------"
+echo "Passed:	$COUNTER_PASSED"
+echo "Total:	$COUNTER_TOTAL"
+echo "----------------"
 
 exit 0
--- gcc-plug/dlsym/tests/symbols-test.sh
+++ gcc-plug/dlsym/tests/symbols-test.sh
@@ -1,153 +1,369 @@
 #!/bin/bash
 PLUGIN_ARG="-fplugin-arg-libplug-sign-dlsym=1 -fplugin-arg-libplug-in=signs.txt"
 
+COUNTER_PASSED=0
+COUNTER_TOTAL=0
+
+function compile_test() {
+  echo "COMPILING $TFILE"
+  $TCOMPILER $TFILE $TFLAGS
+}
+
 function dump_contains() {
   echo $(grep -cPR "$2" $1)
 }
 
-function simple_test() {
-  local file="$1"
+function dump_check_test() {
+  local name="$1"
   local str="$2"
   local expected="$3"
-  echo "TESTING $file ($4)"
-  $CC -O2 -fplugin=$PLUGIN $PLUGIN_ARG $file -ldl -fdump-ipa-all
-  local actual=$(dump_contains $file.*i.symbols "$str")
+  COUNTER_TOTAL=$((COUNTER_TOTAL + 1))
+  printf "TESTING $TFILE ($name):"
+  local actual=$(dump_contains $TFILE.*i.$TPASS "$str")
   if [ $expected -eq $actual ]
   then
+    COUNTER_PASSED=$((COUNTER_PASSED + 1))
     echo "PASS"
   else
     echo "FAIL: Expected $expected but actual is $actual"
   fi
-  rm $file.* *.out
 }
 
-function simple_test_cpp() {
-  local file="$1"
-  local str="$2"
-  local expected="$3"
-  echo "TESTING $file ($4)"
-  $CXX -O2 -fplugin=$PLUGIN $PLUGIN_ARG $file -ldl -fdump-ipa-all
-  local actual=$(dump_contains $file.*i.symbols "$str")
-  if [ $expected -eq $actual ]
-  then
-    echo "PASS"
-  else
-    echo "FAIL: Expected $expected but actual is $actual"
-  fi
-  rm $file.* *.out
+function cleanup_test() {
+  rm $TFILE.* *.out
 }
 
+# symbols-pass.c
+TPASS="symbols"
 echo "------------------"
 echo "symbols-pass tests"
+echo "------------------"
+
+TFLAGS="-O2 -fplugin=$PLUGIN $PLUGIN_ARG -fdump-ipa-all -ldl"
+
+## C tests
+TCOMPILER=$CC
+
+### array_assign.c
+TFILE="array_assign.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[init\]" 1
+cleanup_test
+
+### array_assign_part.c
+TFILE="array_assign_part.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "part-const" "dlsym set state:PARTIALLY_CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[_init\]" 1
+cleanup_test
+
+### asm_name.c
+TFILE="asm_name.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "wrong symbol" "\*asm_func_caller->dlsym->\[bar\]" 0
+dump_check_test "symbol" "asm_func_caller->dlsym->\[bar\]" 1
+cleanup_test
+
+### const_array.c
+TFILE="const_array.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[bar\]" 1
+cleanup_test
+
+### const_array2.c
+TFILE="const_array2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(foo,zoo)|(zoo,foo)\]" 1
+cleanup_test
+
+### const_array3.c
+TFILE="const_array3.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1
+cleanup_test
+
+### const_array4.c
+TFILE="const_array4.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1
+cleanup_test
+
+### const_array5.c
+TFILE="const_array5.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "part-const" "dlsym set state:PARTIALLY_CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1
+cleanup_test
+
+### const_array6.c
+TFILE="const_array6.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(bar1,bar)|(bar,bar1)\]" 1
+cleanup_test
+
+### const_array7.c
+TFILE="const_array7.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "dyn" "dlsym set state:DYNAMIC" 1
+cleanup_test
+
+### cycle_cgraph.c
+TFILE="cycle_cgraph.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "caller2->dlsym->\[bar\]" 1
+cleanup_test
+
+### cycle_cgraph2.c
+TFILE="cycle_cgraph2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller->dlsym->\[bar\]" 1
+cleanup_test
+
+### const_struct.c
+TFILE="const_struct.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "sym" "main->dlsym->\[(baz,bar)|(bar,baz)\]" 1
+cleanup_test
+
+### conditional.c
+TFILE="conditional.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "sym" "main->dlsym->\[(baz,bar)|(bar,baz)\]" 1
+cleanup_test
+
+### dlvsym.c
+TFILE="dlvsym.c"
+
+compile_test
+dump_check_test "sign" "dlvsym matched to the signature" 1
+dump_check_test "const" "dlvsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlvsym->\[bar\]" 1
+cleanup_test
+
+### func_call.c
+TFILE="func_call.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "dyn" "dlsym set state:DYNAMIC" 1
+cleanup_test
+
+### func_macro.c
+TFILE="func_macro.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "foo->dlsym->\[foo\]" 1
+cleanup_test
+
+### global_var.c
+TFILE="global_var.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[foo\]" 1
+cleanup_test
+
+### global_var2.c
+TFILE="global_var2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "part-const" "dlsym set state:PARTIALLY_CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[(foo,goo)|(goo,foo)\]" 1
+cleanup_test
+
+### ifcallopt.c
+TFILE="ifcallopt.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 2
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "undef" "dlsym set state:UNDEFINED" 1
+dump_check_test "symbol" "function_caller.constprop.0->dlsym->\[func_name\]" 1
+cleanup_test
+
+### local_array.c
+TFILE="local_array.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[bar\]" 1
+cleanup_test
+
+### macro_passing.c
+TFILE="macro_passing.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 2
+dump_check_test "const" "dlsym set state:CONSTANT" 2
+dump_check_test "symbol" "main->dlsym->\[foo\]" 1
+dump_check_test "symbol" "main->dlsym->\[bar\]" 1
+cleanup_test
+
+### static_proxy.c
+TFILE="static_proxy.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller.constprop.0->dlsym->\[bar\]" 1
+cleanup_test
+
+### static_proxy1.c
+TFILE="static_proxy1.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller.constprop.0->dlsym->\[(bar,baz)|(baz,bar)\]" 1
+cleanup_test
+
+### static_proxy2.c
+TFILE="static_proxy2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller.constprop.1->dlsym->\[baz\]" 1
+cleanup_test
+
+### switch.c
+TFILE="switch.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "sym" "main->dlsym->\[(baz,foo)|(foo,baz)\]" 1
+cleanup_test
+
+### switch2.c
+TFILE="switch2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller.constprop.0->dlsym->\[(foo,baz)|(baz,foo)\]" 1
+cleanup_test
+
+### undef.c
+TFILE="undef.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "undef" "dlsym set state:UNDEFINED" 1
+cleanup_test
+
+### var.c
+TFILE="var.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[bar\]" 1
+cleanup_test
+
+### wrapper.c
+TFILE="wrapper.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller->dlsym->\[(bar,baz)|(baz,bar)\]" 1
+cleanup_test
+
+### wrapper2.c
+TFILE="wrapper2.c"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "function_caller_1->dlsym->\[(bar,baz)|(baz,bar)\]" 1
+cleanup_test
+
+## C++ tests
+TCOMPILER=$CXX
+
+### const.cpp
+TFILE="const.cpp"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[Create\]" 1
+cleanup_test
+
+### overridden.cpp
+TFILE="overridden.cpp"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "_ZN12interseption22GetRealFunctionAddressEPKc->dlsym->\[foo\]" 1
+cleanup_test
+
+### reinter_cast.cpp
+TFILE="reinter_cast.cpp"
+
+compile_test
+dump_check_test "sign" "dlsym matched to the signature" 1
+dump_check_test "const" "dlsym set state:CONSTANT" 1
+dump_check_test "symbol" "main->dlsym->\[bar\]" 1
+cleanup_test
+
+# Summary
+echo "------------------"
+echo "Passed:	$COUNTER_PASSED"
+echo "Total:	$COUNTER_TOTAL"
 echo "------------------"
 
 # TODO handle constant index
 # echo $(simple_test array.c       "dlsym matched to the signature" 1 "Signature"  )
 # echo $(simple_test array.c       "dlsym set state:DYNAMIC"	   1 "DYNAMIC")
-echo $(simple_test array_assign.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test array_assign.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test array_assign.c "main->dlsym->\[init\]" 1 "Symbol Set" )
-echo $(simple_test array_assign_part.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test array_assign_part.c "dlsym set state:PARTIALLY_CONSTANT" 1 "PARTIALLY_CONSTANT" )
-echo $(simple_test array_assign_part.c "main->dlsym->\[_init\]" 1 "Symbol Set" )
-echo $(simple_test asm_name.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test asm_name.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test asm_name.c "\*asm_func_caller->dlsym->\[bar\]" 0 "Wrong Symbol Set" )
-echo $(simple_test asm_name.c "asm_func_caller->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test_cpp const.cpp "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test_cpp const.cpp "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test_cpp const.cpp "main->dlsym->\[Create\]" 1 "Symbol Set" )
-echo $(simple_test_cpp overridden.cpp "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test_cpp overridden.cpp "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test_cpp overridden.cpp "_ZN12interseption22GetRealFunctionAddressEPKc->dlsym->\[foo\]" 1 "Symbol Set" )
-echo $(simple_test const_array.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_array.c "main->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test const_array2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array2.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_array2.c "main->dlsym->\[(foo,zoo)|(zoo,foo)\]" 1 "Symbol Set" )
-echo $(simple_test const_array3.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array3.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_array3.c "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1 "Symbol Set" )
-echo $(simple_test const_array4.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array4.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_array4.c "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1 "Symbol Set" )
-echo $(simple_test const_array5.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array5.c "dlsym set state:PARTIALLY_CONSTANT" 1 "PARTIALLY_CONSTANT")
-echo $(simple_test const_array5.c "main->dlsym->\[(foo,bar)|(bar,foo)\]" 1 "Symbol Set" )
-echo $(simple_test const_array6.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array6.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_array6.c "main->dlsym->\[(bar1,bar)|(bar,bar1)\]" 1 "Symbol Set" )
-echo $(simple_test const_array7.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_array7.c "dlsym set state:DYNAMIC" 1 "DYNAMIC")
-echo $(simple_test const_struct.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test const_struct.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test const_struct.c "main->dlsym->\[(baz,bar)|(bar,baz)\]" 1 "Symbol Set" )
-echo $(simple_test conditional.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test conditional.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test conditional.c "main->dlsym->\[(baz,bar)|(bar,baz)\]" 1 "Symbol Set" )
 # TODO c_str_call.cpp
-echo $(simple_test cycle_cgraph.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test cycle_cgraph.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test cycle_cgraph.c "caller2->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test cycle_cgraph2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test cycle_cgraph2.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test cycle_cgraph2.c "function_caller->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test dlvsym.c "dlvsym matched to the signature" 1 "Signature")
-echo $(simple_test dlvsym.c "dlvsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test dlvsym.c "main->dlvsym->\[bar\]" 1 "Symbol Set")
-echo $(simple_test func_call.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test func_call.c "dlsym set state:DYNAMIC" 1 "DYNAMIC")
-echo $(simple_test func_macro.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test func_macro.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test func_macro.c "foo->dlsym->\[foo\]" 1 "Symbol Set" )
-echo $(simple_test global_var.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test global_var.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test global_var.c "main->dlsym->\[foo\]" 1 "Symbol Set" )
-echo $(simple_test global_var2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test global_var2.c "dlsym set state:PARTIALLY_CONSTANT" 1 "PARTIALLY_CONSTANT")
-echo $(simple_test global_var2.c "main->dlsym->\[(foo,goo)|(goo,foo)\]" 1 "Symbol Set" )
-echo $(simple_test ifcallopt.c "dlsym matched to the signature" 2 "Signature"  )
-echo $(simple_test ifcallopt.c "dlsym set state:CONSTANT" 1 "CONSTANT")
-echo $(simple_test ifcallopt.c "dlsym set state:UNDEFINED" 1 "UNDEFINED")
-echo $(simple_test ifcallopt.c "function_caller.constprop.0->dlsym->\[func_name\]" 1 "Symbol Set" )
-echo $(simple_test local_array.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test local_array.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test local_array.c "main->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test macro_passing.c "dlsym matched to the signature" 2 "Signature"  )
-echo $(simple_test macro_passing.c "dlsym set state:CONSTANT" 2 "CONSTANT" )
-echo $(simple_test macro_passing.c "main->dlsym->\[foo\]" 1 "Symbol Set" )
-echo $(simple_test macro_passing.c "main->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test_cpp reinter_cast.cpp "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test_cpp reinter_cast.cpp "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test_cpp reinter_cast.cpp "main->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test static_proxy.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test static_proxy.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test static_proxy.c "function_caller.constprop.0->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test static_proxy1.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test static_proxy1.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test static_proxy1.c "function_caller.constprop.0->dlsym->\[(baz,bar)|(bar,baz)\]" 1 "Symbol Set" )
-echo $(simple_test static_proxy2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test static_proxy2.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test static_proxy2.c "function_caller.constprop.1->dlsym->\[baz\]" 1 "Symbol Set" )
 # TODO strcat.c
-echo $(simple_test switch.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test switch.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test switch.c "main->dlsym->\[(baz,foo)|(foo,baz)\]" 1 "Symbol Set" )
-echo $(simple_test switch2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test switch2.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test switch2.c "function_caller.constprop->dlsym->\[(baz,foo)|(foo,baz)\]" 1 "Symbol Set" )
-echo $(simple_test undef.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test undef.c "dlsym set state:UNDEFINED" 1 "UNDEFINED" )
-echo $(simple_test var.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test var.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test var.c "main->dlsym->\[bar\]" 1 "Symbol Set" )
-echo $(simple_test wrapper.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test wrapper.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test wrapper.c "function_caller->dlsym->\[(bar,baz)|(baz,bar)\]" 1 "Symbol Set" )
-echo $(simple_test wrapper2.c "dlsym matched to the signature" 1 "Signature"  )
-echo $(simple_test wrapper2.c "dlsym set state:CONSTANT" 1 "CONSTANT" )
-echo $(simple_test wrapper2.c "function_caller_1->dlsym->\[(bar,baz)|(baz,bar)\]" 1 "Symbol Set" )
 #echo $(simple_test wrapper_complex.c "dlsym matched to the signature" 1 "Signature"  )
 #echo $(simple_test wrapper_complex.c "dlsym set state:DYNAMIC" 1 "DYNAMIC" )
 #echo $(simple_test wrapper_complex.c "function_caller->dlsym->\[bar\]" 1 "Symbol Set" )
