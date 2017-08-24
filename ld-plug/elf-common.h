2d2f6a9b369f2ff07897f998235b7eb6ff388cc1
Alexander Monakov <amonakov@ispras.ru>
2017-08-24 17:08:48 +0300

    ld-plug: factor out common macros to elf-common.h

---
+++ ld-plug/elf-common.h
@@ -0,0 +1,43 @@
+#ifndef ELF_COMMON_H
+#define ELF_COMMON_H
+
+#include "confdef.h"
+
+/* Some versions of the elf headers define it as signed int.  */
+#undef SHF_EXCLUDE
+#define SHF_EXCLUDE	     (1U << 31)
+
+#ifndef PLUG_TARGET_ELFCLASS
+#if UINTPTR_MAX == UINT64_MAX
+#define PLUG_TARGET_ELFCLASS 64
+#elif UINTPTR_MAX == UINT32_MAX
+#define PLUG_TARGET_ELFCLASS 32
+#endif
+#endif
+
+#if PLUG_TARGET_ELFCLASS == 64
+#define ElfNN_(t) Elf64_##t
+#define ELFCLASS ELFCLASS64
+#define ELF_ST_BIND(st_info)        ELF64_ST_BIND(st_info)
+#define ELF_ST_VISIBILITY(st_other) ELF64_ST_VISIBILITY(st_other)
+#define ELF_R_SYM(r_info)           ELF64_R_SYM(r_info)
+#elif PLUG_TARGET_ELFCLASS == 32
+#define ElfNN_(t) Elf32_##t
+#define ELFCLASS ELFCLASS32
+#define ELF_ST_BIND(st_info)        ELF32_ST_BIND(st_info)
+#define ELF_ST_VISIBILITY(st_other) ELF32_ST_VISIBILITY(st_other)
+#define ELF_R_SYM(r_info)           ELF32_R_SYM(r_info)
+#endif
+
+#define ELFDATA ELFDATA2LSB
+
+#define PLUG_SECTION_PREFIX ".comment.privplugid."
+
+static const char *
+plug_srcid(const char *name)
+{
+	if (strncmp (name, PLUG_SECTION_PREFIX, strlen (PLUG_SECTION_PREFIX)))
+		return 0;
+	return name + strlen (PLUG_SECTION_PREFIX);
+}
+#endif
--- ld-plug/plug.c
+++ ld-plug/plug.c
@@ -7,42 +7,12 @@
 
 #include <elf.h>
 
+#include "elf-common.h"
 #include "md5.h"
-#include "confdef.h"
-
-/* Some versions of the elf headers define it as signed int.  */
-#undef SHF_EXCLUDE
-#define SHF_EXCLUDE	     (1U << 31)
 
 #define HAVE_STDINT_H
 #include <plugin-api.h>
 _Static_assert(LD_PLUGIN_API_VERSION == 1, "unexpected plugin API version");
-
-#ifndef PLUG_TARGET_ELFCLASS
-#if UINTPTR_MAX == UINT64_MAX
-#define PLUG_TARGET_ELFCLASS 64
-#elif UINTPTR_MAX == UINT32_MAX
-#define PLUG_TARGET_ELFCLASS 32
-#endif
-#endif
-
-#if PLUG_TARGET_ELFCLASS == 64
-#define ElfNN_(t) Elf64_##t
-#define ELFCLASS ELFCLASS64
-#define ELF_ST_BIND(st_info)        ELF64_ST_BIND(st_info)
-#define ELF_ST_VISIBILITY(st_other) ELF64_ST_VISIBILITY(st_other)
-#define ELF_R_SYM(r_info)           ELF64_R_SYM(r_info)
-#elif PLUG_TARGET_ELFCLASS == 32
-#define ElfNN_(t) Elf32_##t
-#define ELFCLASS ELFCLASS32
-#define ELF_ST_BIND(st_info)        ELF32_ST_BIND(st_info)
-#define ELF_ST_VISIBILITY(st_other) ELF32_ST_VISIBILITY(st_other)
-#define ELF_R_SYM(r_info)           ELF32_R_SYM(r_info)
-#endif
-
-#define ELFDATA ELFDATA2LSB
-
-#define PLUG_SECTION_PREFIX ".comment.privplugid."
 
 static int sockfd;
 static struct {
@@ -348,13 +318,12 @@ dg_object_end()
 {
 }
 
-static int
+static const char *
 dg_record_srcid(const char *name)
 {
-	if (strncmp (name, PLUG_SECTION_PREFIX, strlen (PLUG_SECTION_PREFIX)))
-		return 0;
-	dg_info.obj_list->srcid = name + strlen (PLUG_SECTION_PREFIX);
-	return 1;
+	const char *srcid = plug_srcid(name);
+	if (srcid) dg_info.obj_list->srcid = srcid;
+	return srcid;
 }
 static void
 dg_gen_srcid(const unsigned char *view, size_t len)
