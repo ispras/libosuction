abcd696c5272ac1c421c4f700d0140278ea2500c
Evgeny <kudryashov@ispras.ru>
2017-11-09 13:32:51 +0300

    util/srcid.c: fix ARM assembler failure For details see 72a226d826a8cc6243c44e8c72294d88f9ed80be

--- util/srcid.c
+++ util/srcid.c
@@ -3,5 +3,5 @@
 /* TODO: hoist PLUG_SECTION_PREFIX definition, #include "common.h" */
 
 asm("\t.pushsection\t" PLUG_SECTION_PREFIX
-    "0123456789abcdef0123456789abcdef,\"e\",@note\n"
+    "0123456789abcdef0123456789abcdef,\"e\",%note\n"
     "\t.popsection\n");
