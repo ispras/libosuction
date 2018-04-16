e7888fa5931ce1e420160a2488c28f8fdfaa91ac
Evgeny <kudryashov@ispras.ru>
2018-04-16 18:31:53 +0300

    Update mv_test.sh to test dlsym plugin with different GCC versions
    Now it is possible to test dlsym plugin separately with different
    GCC versions.
    
    gcc-plug/dlsym/mv_test.sh: update script to current make structure

--- gcc-plug/dlsym/mv_test.sh
+++ gcc-plug/dlsym/mv_test.sh
@@ -2,24 +2,29 @@
 
 # It is a multi-version testing script, for checking plugin with different
 # GCC versions.
+COMPILER_PATH=/mnt/wspace/Workspace/install
 
 # GCC 4.9.0
-CC="/home/eugene/Workspace/install/gcc-4_9_0/bin/x86_64-unknown-linux-gnu-gcc"
-CXX="/home/eugene/Workspace/install/gcc-4_9_0/bin/x86_64-unknown-linux-gnu-g++"
+CC="$COMPILER_PATH/gcc-4_9_0/bin/x86_64-unknown-linux-gnu-gcc"
+CXX="$COMPILER_PATH/gcc-4_9_0/bin/x86_64-unknown-linux-gnu-g++"
+make -C ../../ CC=$CC CXX=$CXX -B
 make test CC=$CC CXX=$CXX -B
 
 # System GCC 5.4.0
 CC=gcc
 CXX=g++
+make -C ../../ CC=$CC CXX=$CXX -B
 make test CC=$CC CXX=$CXX -B
 
 # GCC 6.1.0
-CC="/home/eugene/Workspace/install/gcc-6_1_0/bin/x86_64-pc-linux-gnu-gcc"
-CXX="/home/eugene/Workspace/install/gcc-6_1_0/bin/x86_64-pc-linux-gnu-g++"
+CC="$COMPILER_PATH/gcc-6_1_0/bin/x86_64-pc-linux-gnu-gcc"
+CXX="$COMPILER_PATH/gcc-6_1_0/bin/x86_64-pc-linux-gnu-g++"
+make -C ../../ CC=$CC CXX=$CXX -B
 make test CC=$CC CXX=$CXX -B
 
-# GCC 8.0.0
-CC="/home/eugene/Workspace/install/gcc-trunk/bin/x86_64-pc-linux-gnu-gcc"
-CXX="/home/eugene/Workspace/install/gcc-trunk/bin/x86_64-pc-linux-gnu-g++"
+# GCC 7.1.0
+CC="$COMPILER_PATH/gcc-7_1_0/bin/x86_64-pc-linux-gnu-gcc"
+CXX="$COMPILER_PATH/gcc-7_1_0/bin/x86_64-pc-linux-gnu-g++"
+make -C ../../ CC=$CC CXX=$CXX -B
 make test CC=$CC CXX=$CXX -B
 
