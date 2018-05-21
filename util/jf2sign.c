d84dc9de52eb3df834b7c41b62d348badba2a451
Evgeny <kudryashov@ispras.ru>
2018-05-21 18:41:09 +0300

    Refactor jf2sign tool
    util/jf2sign.c: decompose code

--- util/jf2sign.c
+++ util/jf2sign.c
@@ -20,78 +20,62 @@ struct list_node
   struct list_node *next;
 };
 
-struct list_node *output;
-struct list_node *current;
-struct list_node *next;
-
 static bool
-has_duplicates (struct jfunction *jf, struct list_node *list)
+has_duplicates(struct jfunction *jf, struct list_node *list)
 {
   struct list_node *iter;
   for (iter = list; iter; iter = iter->next)
-    if (!strcmp (jf->from_name, iter->jf->from_name)
+    if (!strcmp(jf->from_name, iter->jf->from_name)
 	&& jf->from_arg == iter->jf->from_arg)
       return true;
   return false;
 }
 
-int main (int argc, char *argv[])
+static struct list_node *
+create_list_node(const char *from_name, int from_arg,
+		 const char *to_name, int to_arg,
+		 struct list_node *next)
 {
-  char* from_name;
+  struct list_node *node = calloc(1, sizeof *node);
+  node->jf = calloc(1, sizeof *node->jf);
+  node->jf->from_name = from_name;
+  node->jf->from_arg = from_arg;
+  node->jf->to_name = to_name;
+  node->jf->to_arg = to_arg;
+  node->next = next;
+  return node;
+}
+
+static struct list_node *
+read_jf(FILE *f)
+{
+  struct list_node *res = NULL;
+  char *from_name, *to_name;
+  int from_arg, to_arg;
+  while (fscanf(f, "%ms %d %ms %d", &from_name, &from_arg, &to_name, &to_arg) != EOF)
+      res = create_list_node(from_name, from_arg, to_name, to_arg, res);
+  return res;
+}
+
+static struct list_node *
+read_base(FILE *f)
+{
+  struct list_node *res = NULL;
+  char *from_name;
   int from_arg;
-  char* to_name;
-  int to_arg;
-  struct list_node *root = NULL, *last = NULL;
+  while (fscanf(f, "%ms %d", &from_name, &from_arg) != EOF)
+      res = create_list_node(from_name, from_arg, NULL, 0, res);
+  return res;
+}
 
-  if (argc < 3)
-    {
-      fprintf (stderr, "Two required arguments should be given:\n"
-	       " 1) a file with jump functions\n"
-	       " 2) a file with base signatures\n"
-	       " 3) [Optional] a file to output resulting signatures.\n");
-      return 1;
-    }
-
-  // Read jump functions
-  FILE *f = fopen (argv[1], "r");
-
-  while (f && fscanf (f, "%ms %d %ms %d", &from_name, &from_arg, &to_name, &to_arg) != EOF)
-    {
-      struct jfunction *temp = (struct jfunction *) malloc (sizeof (struct jfunction));
-      temp->from_name = from_name;
-      temp->from_arg = from_arg;
-      temp->to_name = to_name;
-      temp->to_arg = to_arg;
-
-      root = (struct list_node *) malloc (sizeof (struct list_node));
-      root->jf = temp;
-      root->next = last;
-
-      last = root;
-    }
-
-  fclose (f);
-
-  // Read initial calls
-  f = fopen (argv[2], "r");
-
-  while (f && fscanf (f, "%ms %d", &from_name, &from_arg) != EOF)
-    {
-      struct list_node *temp = (struct list_node *) malloc (sizeof (struct list_node));
-      temp->jf = (struct jfunction *) malloc (sizeof (struct jfunction));
-      temp->jf->from_name = from_name;
-      temp->jf->from_arg = from_arg;
-      temp->next = current;
-      current = temp;
-    }
-
-  fclose (f);
-
-  // Match jump function chains
+static struct list_node *
+find_closure(struct list_node *start, struct list_node *pool)
+{
+  struct list_node *current = start, *next = NULL, *output = NULL;
   while (current)
     {
       struct list_node *temp;
-      for (temp = root; temp; temp = temp->next)
+      for (temp = pool; temp; temp = temp->next)
 	{
 	  struct list_node *cur_iter;
 	  for (cur_iter = current; cur_iter; cur_iter = cur_iter->next)
@@ -100,13 +84,13 @@ int main (int argc, char *argv[])
 		  && temp->jf->to_arg == cur_iter->jf->from_arg)
 		{
 		  /* Check duplicates in three disjoint lists */
-		  if (has_duplicates (temp->jf, next) ||
-		      has_duplicates (temp->jf, output) ||
-		      has_duplicates (temp->jf, current))
+		  if (has_duplicates(temp->jf, next)
+		      || has_duplicates(temp->jf, output)
+		      || has_duplicates(temp->jf, current))
 		    break;
 
 		  // Include jf to dlsym signatures
-		  struct list_node *nw = (struct list_node *) malloc (sizeof (struct list_node));
+		  struct list_node *nw = calloc(1, sizeof *nw);
 		  nw->jf = temp->jf;
 		  nw->next = next;
 		  next = nw;
@@ -115,7 +99,7 @@ int main (int argc, char *argv[])
 	    }
 	}
 
-      /* Signatures from CURRENT move to the end of OUTPUT;
+      /* Signatures from CURRENT move to the beginning of OUTPUT;
 	 signatures from NEXT move to CURRENT.
 	 If NEXT is empty, therefore we did not find anything, stop the search
 	 else set NEXT to zero and continue, because list of signatures has
@@ -131,34 +115,59 @@ int main (int argc, char *argv[])
       current = next;
       next = NULL;
     }
+  return output;
+}
+
+int main (int argc, char *argv[])
+{
+  struct list_node *base_dlsym, *root, *output;
+  FILE *f;
+
+  if (argc < 3)
+    {
+      fprintf(stderr, "Two required arguments should be given:\n"
+	      " 1) a file with jump functions\n"
+	      " 2) a file with base signatures\n"
+	      " 3) [Optional] a file to output resulting signatures.\n");
+      return 1;
+    }
+
+  // Read jump functions
+  f = fopen(argv[1], "r");
+  if (!f) return 1;
+  root = read_jf(f);
+  fclose(f);
+
+  // Read initial calls
+  f = fopen(argv[2], "r");
+  if (!f) return 1;
+  base_dlsym = read_base(f);
+  fclose(f);
+
+  // Match jump function chains
+  output = find_closure(base_dlsym, root);
 
   // Output results
-  FILE *outf = argc < 4 ? stdout : fopen (argv[3], "w");
+  FILE *outf = argc < 4 ? stdout : fopen(argv[3], "w");
 
-  last = output;
-  while (last)
-    {
-      fprintf (outf, "%s %d\n", last->jf->from_name, last->jf->from_arg);
-      last = last->next;
-    }
+  for (struct list_node *iter = output; iter; iter = iter->next)
+    fprintf(outf, "%s %d\n", iter->jf->from_name, iter->jf->from_arg);
 
   if (argc >= 4)
-    fclose (outf);
+    fclose(outf);
 
   // Free memory
-  while (root)
-    {
-      last = root;
-      root = root->next;
-      free (last->jf);
-      free (last);
-    }
-  while (output)
-    {
-      last = output;
-      output = output->next;
-      free (last);
-    }
+  for (struct list_node *iter = root, *tmp; iter; iter = tmp) {
+    tmp = iter->next;
+    free(iter->jf);
+    free(iter);
+  }
+
+  // All jfuncs are already freed
+  for (struct list_node *iter = output, *tmp; iter; iter = tmp) {
+    tmp = iter->next;
+    free(iter);
+  }
 
   return 0;
 }
